7766aea814d59169c4241c3c26e2f7fb701eb387
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-18 11:43:44 +0600

    sidebar doen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,9 +17,8 @@ export default function Sidebar() {
         <motion.aside
             initial={false}
             animate={{ width: open ? 220 : 64 }}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
             className={`relative bg-slate-800 text-white h-screen transition-width overflow-hidden ${open ? "p-4" : "p-3"}`}
-            style={{ minWidth: open ? 220 : 64 }}
         >
             <div className="px-3">
                 <Link href="/" className="text-2xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-amber-500 to-sky-500">{open ? "Zetta Inc." : "Z"}</Link>
@@ -29,27 +28,27 @@ export default function Sidebar() {
                     href="/"
                     className={`${linkClasses} ${pathname === '/' ? activeLinkClasses : 'bg-slate-700'}`}
                 >
-                    <span className="relative z-10">{open ? "Dashboard" : <MdDashboard />}</span>
+                    <span className="relative z-10">{open ? <span className=' flex items-center gap-x-2'><MdDashboard /> Dashboard</span> : <MdDashboard />}</span>
                 </Link>
 
                 <Link
                     href="/posts"
                     className={`${linkClasses} ${pathname === '/posts' ? activeLinkClasses : 'bg-slate-700'}`}
                 >
-                    <span className="relative z-10">{open ? "Posts" : <FaSignsPost />}</span>
+                    <span className="relative z-10">{open ? <span className=' flex items-center gap-x-2'><FaSignsPost /> Posts</span> : <FaSignsPost />}</span>
                 </Link>
 
                 <Link
                     href="/users"
                     className={`${linkClasses} ${pathname === '/users' ? activeLinkClasses : 'bg-slate-700'}`}
                 >
-                    <span className="relative z-10">{open ? "Users" : <FaUsers />}</span>
+                    <span className="relative z-10">{open ? <span className=' flex items-center gap-x-2'><FaUsers /> Users</span> : <FaUsers />}</span>
                 </Link>
             </nav>
 
             <button
                 onClick={() => setOpen(!open)}
-                className="absolute top-1/2 -translate-1/2 -right-1 w-4 h-8 flex items-center justify-center bg-slate-700 text-slate-100 rounded-full shadow-md hover:bg-slate-500 cursor-pointer"
+                className={`absolute bottom-3 ${open ? "right-2" : "right-3"} size-10 flex items-center justify-center bg-slate-700 text-slate-100 rounded-full shadow-md hover:bg-slate-500 cursor-pointer`}
             >
                 {open ? <BiChevronLeft size={18} /> : <BiChevronRight size={18} />}
             </button>
