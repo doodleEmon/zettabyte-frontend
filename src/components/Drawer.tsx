e452119425b8ce69d11113af70afeace329affbc
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-19 17:15:49 +0600

    drawer created

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import { IoClose } from "react-icons/io5";
+import ReusableButton from "./ReusableButton";
+
+type DrawerProps = {
+    isOpen: boolean;
+    onClose: () => void;
+};
+
+export default function Drawer({ isOpen, onClose }: DrawerProps) {
+    return (
+        <AnimatePresence>
+            {isOpen && (
+                <>
+                    <motion.div
+                        className="fixed inset-0 bg-black/40 z-40"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        onClick={onClose}
+                    />
+
+                    <motion.div
+                        className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-50 p-4"
+                        initial={{ x: -300 }}
+                        animate={{ x: 0 }}
+                        exit={{ x: -300 }}
+                        transition={{ type: "tween", duration: 0.3 }}
+                    >
+                        <div className="flex items-center justify-between mb-2">
+                            <h2 className="text-2xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-amber-500 to-sky-500">Zetta Inc.</h2>
+                            <button onClick={onClose}>
+                                <IoClose size={24} />
+                            </button>
+                        </div>
+                        <hr className="my-3 text-slate-400" />
+
+                        <ul className="space-y-4">
+                            <li><Link href="/" onClick={onClose} className="hover:text-blue-600 font-semibold">Dashboard</Link></li>
+                            <li><Link href="/posts" onClick={onClose} className="hover:text-blue-600 font-semibold">Posts</Link></li>
+                            <li><Link href="/users" onClick={onClose} className="hover:text-blue-600 font-semibold">Users</Link></li>
+                            <li onClick={onClose}><ReusableButton title="Logout" /></li>
+                        </ul>
+                    </motion.div>
+                </>
+            )}
+        </AnimatePresence>
+    );
+}
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,14 @@
 "use client"
 
 import React, { useState } from 'react'
-import ReusableButton from '../ReusableButton'
+import ReusableButton from '@/components/ReusableButton'
+import Image from 'next/image';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import Drawer from '@/components/Drawer';
 
 export default function Header() {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     return (
         <div className='bg-slate-200 px-2'>
@@ -14,10 +18,40 @@ export default function Header() {
                 </div>
                 <div className='flex items-center gap-x-2'>
                     {
-                        isAuthenticated ? <ReusableButton title='Logout' /> : <ReusableButton title='Login' />
+                        isAuthenticated ? <div>
+                            <div className='hidden lg:flex items-center gap-x-3'>
+                                <div className="size-10 rounded-full overflow-hidden border">
+                                    <Image
+                                        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+                                        alt="Profile picture"
+                                        width={40}
+                                        height={40}
+                                        className="object-contain"
+                                    />
+                                </div>
+                                <p>Welcome, Mr. John Doe</p>
+                                <ReusableButton title='Logout' />
+                            </div>
+                            <div className='flex items-center gap-x-3 lg:hidden'>
+                                <div className="size-[30px] rounded-full overflow-hidden border">
+                                    <Image
+                                        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+                                        alt="Profile picture"
+                                        width={30}
+                                        height={30}
+                                        className="object-contain"
+                                    />
+                                </div>
+                                <p className='hidden md:block'>Welcome, Mr. John Doe</p>
+                                <button onClick={() => setIsDrawerOpen(true)}>
+                                    <GiHamburgerMenu size={24} />
+                                </button>
+                            </div>
+                        </div> : <ReusableButton title='Login' />
                     }
                 </div>
             </div>
+            <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
         </div>
     )
 }
